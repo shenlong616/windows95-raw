75739d8e0f04a0029e1af03e9d7b921baa01f091
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-02-04 19:01:05 +0700

    test

--- src/hooks/useControls/Control1.js
+++ src/hooks/useControls/Control1.js
@@ -3,6 +3,7 @@ import themes from "react95/dist/themes";
 
 export function Control1() {
   return useControls("react95", {
+    font: true,
     theme: {
       value: "original",
       options: Object.keys(themes),
@@ -11,6 +12,5 @@ export function Control1() {
       value: "bottom",
       options: ["top"],
     },
-    font: true,
   });
 }
