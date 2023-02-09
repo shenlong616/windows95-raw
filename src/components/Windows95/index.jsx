bb6a5e72d4af935f3b2d113b46baac19dd523ff2
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-02-09 13:01:20 +0700

    3

--- src/components/Windows95/index.jsx
+++ src/components/Windows95/index.jsx
@@ -2,9 +2,11 @@ import { _ } from "./_";
 import { Providers_ } from "../Providers";
 
 function Windows95() {
-  <Providers_.React95>
-    <_.Desktop />
-  </Providers_.React95>;
+  return (
+    <Providers_.React95>
+      <_.Desktop />
+    </Providers_.React95>
+  );
 }
 
 export { Windows95, _ as Windows95_ };
