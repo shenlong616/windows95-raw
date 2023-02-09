7d0fdac49af8e41376ebafd3cc42b2968d39d2f9
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-02-09 08:13:44 +0700

    test

--- src/components/Windows95/Icon.jsx
+++ src/components/Windows95/Icon.jsx
@@ -1,6 +1,7 @@
 import "@react95/icons/icons.css";
 import { clsx } from "clsx";
 
+// https://react95.github.io/React95/?path=/story/icon--all
 export function Icon({ iconName, className }) {
   return (
     <span
