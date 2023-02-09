17a488667d5bb6b43e38ad5ed56a87351c8ed9e1
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-02-09 09:00:23 +0700

    f

--- src/components/Windows95/Desktop/index.js
+++ src/components/Windows95/Desktop/index.js
@@ -1,5 +1,34 @@
 import { _ } from "./_";
+import { Styled_ } from "../../Styled";
+import { clsx } from "clsx";
+import { useControls } from "../../../hooks/useControls";
 
-function Desktop() {}
+function Desktop() {
+  const { control1 } = useControls();
+  const { taskbar } = control1();
+
+  return (
+    <Styled_.Theme
+      cssPropName="background-color"
+      themeColorName="desktopBackground"
+    >
+      <div
+        className={clsx({
+          "flex max-h-screen min-h-screen": true,
+          "flex-col": taskbar === "bottom",
+          "flex-col-reverse": taskbar === "top",
+        })}
+      >
+        <div className="grow overflow-auto p-6">
+          <_.IconList />
+        </div>
+        <_.Taskbar />
+        {/* <div className="absolute flex h-full w-full flex-col items-center justify-center">
+            center element
+          </div> */}
+      </div>
+    </Styled_.Theme>
+  );
+}
 
 export { Desktop, _ as Desktop_ };
