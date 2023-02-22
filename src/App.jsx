10a8980d3f0df3a348139734779b748016c3b103
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-02-22 17:58:44 +0700

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
 import { Uncategorized } from "./components/Uncategorized";
 import { lazy, Suspense } from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const Windows95 = lazy(() => import("./components/Windows95"));
 
 function App() {
   return (
     <>
-      <Uncategorized.HeadTag />
+      <HelmetProvider>
+        <Helmet>
+          <meta
+            // https://stackoverflow.com/questions/2989263/disable-auto-zoom-in-input-text-tag-safari-on-iphone
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+          />
+        </Helmet>
+      </HelmetProvider>
       <Suspense fallback={<div>Loading...</div>}>
         <Windows95 />
       </Suspense>
