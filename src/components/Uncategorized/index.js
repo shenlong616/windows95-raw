21650a42057534d42d2ba7c6adaa80f5c15ea343
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-02-24 13:56:04 +0700

    3

--- src/components/Uncategorized/index.js
+++ src/components/Uncategorized/index.js
@@ -1,8 +1,8 @@
+import { AnimatedIcon } from "./AnimatedIcon";
 import { Draggable } from "./Draggable";
 import { GitHub } from "./GitHub";
-import { AnimatedIcon } from "./AnimatedIcon";
 import { MenuListItem } from "./MenuListItem";
 
 export function Uncategorized() {}
 
-Object.assign(Uncategorized, { Draggable, GitHub, AnimatedIcon, MenuListItem });
+Object.assign(Uncategorized, { AnimatedIcon, Draggable, GitHub, MenuListItem });
