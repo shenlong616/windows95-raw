855495fb9750347c7805474a4b360fdf1b298eb4
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-02-24 18:01:27 +0700

    3

--- src/components/Windows95/index.jsx
+++ src/components/Windows95/index.jsx
@@ -4,9 +4,7 @@ import { Provider } from "../Provider";
 export default function Windows95() {
   return (
     <Provider.React95>
-      <Windows95.Desktop />
+      <Desktop />
     </Provider.React95>
   );
 }
-
-Object.assign(Windows95, { Desktop });
