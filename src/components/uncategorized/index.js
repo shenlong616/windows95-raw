657cd8bb11b8ad5b708e0963d79ffa748ccc04f7
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-02-25 14:10:00 +0700

    3

--- src/components/uncategorized/index.js
+++ src/components/uncategorized/index.js
@@ -2,7 +2,7 @@ import { AnimatedIcon } from "./animated-icon";
 import { Draggable } from "./draggable";
 import { GitHub } from "./github";
 import { Icon } from "./icon";
-import { MenuListItem } from "./MenuListItem";
+import { MenuListItem } from "./menu-list-item";
 import { Text } from "./Text";
 
 export function Uncategorized() {}
