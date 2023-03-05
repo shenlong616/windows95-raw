8c4337217dfa03336d9af3e89300df19a19a2bbd
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-03-05 20:32:45 +0700

    f

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,22 @@
 import "@/styles/globals.css";
 import { Provider } from "@/components/provider";
+import Head from "next/head";
+import { GitHub } from "@/github";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider.React95>
-      <Component {...pageProps} />
-    </Provider.React95>
+    <>
+      <Head>
+        <meta
+          // https://stackoverflow.com/questions/2989263/disable-auto-zoom-in-input-text-tag-safari-on-iphone
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+        />
+      </Head>
+      <Provider.React95>
+        <Component {...pageProps} />
+      </Provider.React95>
+      <GitHub url="https://github.com/shenlong616/windows95-raw" />
+    </>
   );
 }
