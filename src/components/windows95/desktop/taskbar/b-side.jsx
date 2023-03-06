f658d1115507ce6ec4225f1e1b47be0a8c982235
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-03-06 08:04:08 +0700

    f

--- src/components/windows95/desktop/taskbar/b-side.jsx
+++ src/components/windows95/desktop/taskbar/b-side.jsx
@@ -5,7 +5,7 @@ import { Windows95 } from "@/components/windows95";
 export function BSide() {
   return (
     <Frame variant="status">
-      <div className="flex min-w-[6rem] flex-row items-center justify-center gap-2 p-1 text-sm">
+      <div className="flex min-w-[6rem] flex-row items-center justify-center gap-2 py-1 text-sm">
         <Windows95.Icon name="Fax_16x16_4" />
         <Windows95.AnimatedIcon
           iconName1="Lights101_16x16_4"
