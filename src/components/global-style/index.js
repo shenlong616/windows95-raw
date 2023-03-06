c0b8cbaa5e3cc2da65a7c3bb82d049bec9d7b938
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-03-06 07:54:49 +0700

    3

--- src/components/global-style/index.js
+++ src/components/global-style/index.js
@@ -1,6 +1,5 @@
-import { CustomFont } from "@/components/global-style/custom-font";
 import { StyleReset } from "@/components/global-style/style-reset";
 
 export function GlobalStyle() {}
 
-Object.assign(GlobalStyle, { CustomFont, StyleReset });
+Object.assign(GlobalStyle, { StyleReset });
