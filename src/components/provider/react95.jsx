dabd8d3fa8579ac0fa135fa81075a5c286cb8f02
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-03-06 07:58:20 +0700

    3

--- src/components/provider/react95.jsx
+++ src/components/provider/react95.jsx
@@ -2,16 +2,12 @@
 import { ThemeProvider } from "styled-components";
 import themes from "react95/dist/themes";
 import { useLeva } from "@/hooks/use-leva";
-import { GlobalStyle } from "@/components/global-style";
 
 export function React95Provider({ children }) {
   let { control1 } = useLeva();
   control1 = control1();
 
   return (
-    <>
-      <GlobalStyle.StyleReset />
-      <ThemeProvider theme={themes[control1.theme]}>{children}</ThemeProvider>
-    </>
+    <ThemeProvider theme={themes[control1.theme]}>{children}</ThemeProvider>
   );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { Provider } from "@/components/provider";
 import Head from "next/head";
 import { GitHub } from "@/github";
 import { Font } from "@/components/font";
+import { GlobalStyle } from "@/components/global-style";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -14,11 +15,14 @@ export default function App({ Component, pageProps }) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
         />
       </Head>
+
+      <GlobalStyle.StyleReset />
       <Font.MsSansSerif>
         <Provider.React95>
           <Component {...pageProps} />
         </Provider.React95>
       </Font.MsSansSerif>
+
       <GitHub url="https://github.com/shenlong616/windows95-raw" />
     </>
   );
