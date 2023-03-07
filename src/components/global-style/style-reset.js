a8f372a54ebeedf3c3186c6045cf7407c45cda65
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-03-07 09:48:31 +0700

    3

--- src/components/global-style/style-reset.js
+++ src/components/global-style/style-reset.js
@@ -1,9 +1,5 @@
 import { styleReset } from "react95";
 
 export function StyleReset() {
-  return (
-    <style jsx global>
-      {styleReset}
-    </style>
-  );
+  return <style jsx>{styleReset}</style>;
 }
