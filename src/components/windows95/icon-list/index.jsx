a813140e58d27deae2b8ee19c125327b78ce86f3
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-03-07 15:38:13 +0700

    d

---
+++ src/components/windows95/icon-list/index.jsx
@@ -0,0 +1,14 @@
+import { Item } from "@/components/windows95/icon-list/item";
+
+export function IconList() {
+  return (
+    // https://codepen.io/rachelandrew/pen/QbewmG
+    <div className="grid auto-cols-max grid-flow-col grid-rows-6 gap-11">
+      <Item iconName="Computer_32x32_4" title="My Computer" />
+      <Item iconName="RecycleEmpty_32x32_4" title="Recycle Bin" />
+      <Item iconName="Mspaint_32x32_4" title="Paint" />
+      <Item iconName="Freecell1_32x32_4" title="FreeCell" />
+      <Item iconName="Folder_32x32_4" title="New folder" />
+    </div>
+  );
+}
