2f5b612531286bfa210faac17c40a328cd431de1
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-03-08 08:28:55 +0700

    f

--- src/hooks/use-leva/index.js
+++ src/hooks/use-leva/index.js
@@ -1,5 +1,5 @@
-import { control1 } from "./control-1";
-import { control2 } from "./control-2";
+import { control1 } from "@/hooks/use-leva/control-1";
+import { control2 } from "@/hooks/use-leva/control-2";
 
 export function useLeva() {
   return { control1, control2 };
