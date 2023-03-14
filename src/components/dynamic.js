9ae38b3f5807c4f294e35de6169cb809441c5346
Shenlong <15698245+Shenlong616@users.noreply.github.com>
2023-03-14 15:06:38 +0700

    test

--- src/components/dynamic.js
+++ src/components/dynamic.js
@@ -1,7 +1,9 @@
 import dynamic from "next/dynamic";
 
 const StyleReset = dynamic(() =>
-  import("@/components/global-style").then((mod) => mod.GlobalStyle.StyleReset)
+  import("@/components/global-style").then(
+    (module) => module.GlobalStyle.StyleReset
+  )
 );
 
 export function Dynamic() {}
